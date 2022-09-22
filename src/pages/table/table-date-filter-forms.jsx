0559823a80911bc77c66a74e3e5a964bf5c72f55
Andrei Zhaleznichenka <andrei.zhaleznichenka@gmail.com>
2022-09-22 13:46:29 +0200

    Update table-date-filter-forms.jsx

--- src/pages/table/table-date-filter-forms.jsx
+++ src/pages/table/table-date-filter-forms.jsx
@@ -80,7 +80,7 @@ function parseDateTimeFilter(filter) {
   const [dateValue, timeValue] = dateTime.split('T');
 
   if (!timeValue) {
-    return { dateValue: isValidIsoDate(dateValue) ? dateValue : '', timeValue: ' ' };
+    return { dateValue: isValidIsoDate(dateValue) ? dateValue : '', timeValue: '' };
   }
 
   const [hours = '00', minutes = '00', seconds = '00'] = timeValue.split(':');
