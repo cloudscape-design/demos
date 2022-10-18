cf3bf65f8b453e10eca67d18f78344d4543ac64b
Andrei Zhaleznichenka <zhalea@amazon.de>
2022-10-18 08:18:44 +0200

    Experiment: revert date provider changes

--- src/pages/commons/data-provider.jsx
+++ src/pages/commons/data-provider.jsx
@@ -2,13 +2,11 @@
 // SPDX-License-Identifier: MIT-0
 export default class DataProvider {
   getData(name) {
-    return fetch(`../resources/${name}.json`)
-      .then(response => {
-        if (!response.ok) {
-          throw new Error(`Response error: ${response.status}`);
-        }
-        return response.json();
-      })
-      .then(data => data.map(it => ({ ...it, date: new Date(it.date) })));
+    return fetch(`../resources/${name}.json`).then(response => {
+      if (!response.ok) {
+        throw new Error(`Response error: ${response.status}`);
+      }
+      return response.json();
+    });
   }
 }
